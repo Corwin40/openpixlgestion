191458b5efeee83ddfa2c41e35547a8b8c3d4826
Corwin40 <xavier.burke@openpixl.fr>
2024-05-20 13:18:27 +0200

    transfert

--- public/js/client/intervention.js
+++ public/js/client/intervention.js
@@ -1,6 +1,55 @@
-// Appel des éléments
+// Appel des éléments liés aux Services
+const btnAddService = document.getElementById('btnAddService');
+// Appel des éléments liés aux interventions
 const modalIntervention = document.getElementById('modalIntervention')
 const btnEditIntervention = document.getElementById('btnEditIntervention')
+
+const modalServ = new bootstrap.Modal(document.getElementById('modalServ'), {keyboard: false})
+const modalServBs = document.getElementById('modalServ')
+
+btnAddService.addEventListener('click', addService)
+modalServBs.addEventListener('hidden.bs.modal', function(){
+    modalServ.querySelector('.modal-dialog').classList.remove('modal-xl', 'modal-dialog-centered')
+    modalServ.querySelector('.modal-header').innerHTML =
+        '<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>\n' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
+    ;
+    modalServ.querySelector('.modal-body').innerHTML =
+        '<div class="d-flex justify-content-center">\n' +
+        '<div class="spinner-border text-primary" role="status">\n' +
+        '<span class="sr-only">Loading...</span>\n' +
+        '</div>\n' +
+        '</div>'
+    ;
+    modalServ.querySelector('.modal-footer').innerHTML =
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>\n' +
+        '<button type="button" class="btn btn-primary">Save changes</button>'
+    ;
+})
+
+function addService(event){
+    event.preventDefault();
+    let opt = this.getAttribute('data-bs-whatever');
+    let crud = opt.split('-')[0];
+    let contentTitle = opt.split('-')[1];
+    let url = this.href;
+    modalServ.show();
+    modalServBs.querySelector('.modal-dialog').classList.add('modal-xl', 'modal-dialog-centered');
+    modalServBs.querySelector('.modal-title').textContent = contentTitle;
+}
+
+// --------------------------------------
+// Code lié aux interventions
+// --------------------------------------
+modalIntervention.addEventListener('hidden.bs.modal', function(){
+    modalIntervention.querySelector('.modal-dialog').classList.remove('modal-xl');
+    modalIntervention.querySelector('.modal-body').innerHTML =
+        '<div class="d-flex justify-content-center">\n' +
+        '<div class="spinner-border text-primary" role="status">\n' +
+        '<span class="sr-only">Loading...</span>\n' +
+        '</div>\n' +
+        '</div>'
+})
 
 modalIntervention.addEventListener('show.bs.modal', function (event){
     // Button that triggered the modal
@@ -11,31 +60,15 @@ modalIntervention.addEventListener('show.bs.modal', function (event){
     let contentTitle = recipient.split('-')[1]
     let id = recipient.split('-')[2]
     let namePage = recipient.split('-')[3]
+    console.log(crud)
     if(crud === 'ADD'){
         let modalHeaderH5 = modalIntervention.querySelector('.modal-title')
         modalHeaderH5.textContent = contentTitle
         axios
             .get('/admin/intervention/addinterveonclient/' + id )
             .then(function (response) {
-                const form = document.getElementById('ModalBodyaddstatutonclient').innerHTML = response.data.form;
-                // Ajout d'un évènement sur le click du selecteur ajout d'un service
-                document.querySelectorAll('a.btnaddonclient').forEach(function (link) {
-                    link.addEventListener('click', btnaddonclient);
-                })
-
-                // Ajout d'un évènement sur le click de suppression service
-                document.querySelectorAll('a.btnSupprService').forEach(function (link) {
-                    link.addEventListener('click', btnSupprService);
-                })
-
-                // Ajout d'un évènement sur le click du selecteur d'un statut
-                document.querySelectorAll('a.btnaddstatutonclient').forEach(function (link) {
-                    link.addEventListener('click', btnaddstatutonclient);
-                })
-
-                document.querySelectorAll('a.btnEditIntervention').forEach(function (link) {
-                    link.addEventListener('click', editIntervention);
-                })
+                const form = document.getElementById('ModalBodyListintervonfiche').innerHTML = response.data.form;
+                reloadInterventionEvent();
             })
             .catch(function (error) {
                 // handle error
@@ -59,28 +92,8 @@ modalIntervention.addEventListener('show.bs.modal', function (event){
         axios
             .get('/admin/intervention/listeinterveonclient/' + id )
             .then(function (response) {
-                let list = document.getElementById('ModalBodyListintervonfiche').innerHTML = response.data.list;
-                // Ajout d'un évènement sur le click du selecteur ajout d'un service
-                document.querySelectorAll('a.btnaddonclient').forEach(function (link) {
-                    link.addEventListener('click', btnaddonclient);
-                })
-
-                // Ajout d'un évènement sur le click de suppression service
-                document.querySelectorAll('a.btnSupprService').forEach(function (link) {
-                    link.addEventListener('click', btnSupprService);
-                })
-
-                // Ajout d'un évènement sur le click du selecteur d'un statut
-                document.querySelectorAll('a.btnaddstatutonclient').forEach(function (link) {
-                    link.addEventListener('click', btnaddstatutonclient);
-                })
-                let urlInvoice = document.getElementById('urlNewInvoice');
-                if (urlInvoice !== null){
-                    document.getElementById('NewInvoice').href = urlInvoice.href;
-                }
-                document.querySelectorAll('a.btnEditIntervention').forEach(function (link) {
-                    link.addEventListener('click', editIntervention);
-                })
+                document.getElementById('ModalBodyListintervonfiche').innerHTML = response.data.list;
+                reloadInterventionEvent();
             })
             .catch(function (error) {
                 // handle error
@@ -106,4 +119,33 @@ function editIntervention(event){
 
         })
     ;
+}
+
+function btnSupprService(event){
+    event.preventDefault()
+}
+
+function reloadInterventionEvent()
+{
+    // Ajout d'un évènement sur le click du selecteur ajout d'un service
+    document.querySelectorAll('a.btnaddonclient').forEach(function (link) {
+        link.addEventListener('click', btnaddonclient);
+    })
+
+    // Ajout d'un évènement sur le click de suppression service
+    document.querySelectorAll('a.btnSupprService').forEach(function (link) {
+        link.addEventListener('click', btnSupprService);
+    })
+
+    // Ajout d'un évènement sur le click du selecteur d'un statut
+    document.querySelectorAll('a.btnaddstatutonclient').forEach(function (link) {
+        link.addEventListener('click', btnaddstatutonclient);
+    })
+    let urlInvoice = document.getElementById('urlNewInvoice');
+    if (urlInvoice !== null){
+        document.getElementById('NewInvoice').href = urlInvoice.href;
+    }
+    document.querySelectorAll('a.btnEditIntervention').forEach(function (link) {
+        link.addEventListener('click', editIntervention);
+    })
 }
